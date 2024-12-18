388a6e2f1cf0a1c2d33486a94616e536066820d7
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2024-12-19 02:17:59 +0300

    Update deploy.sh

--- painfx_backend/backend/deploy.sh
+++ painfx_backend/backend/deploy.sh
@@ -9,19 +9,19 @@ echo "POSTGRES_PASSWORD: $POSTGRES_PASSWORD"
 echo "POSTGRES_HOST: $POSTGRES_HOST"
 echo "POSTGRES_PORT: $POSTGRES_PORT"
 
-# Function to check database readiness
-function wait_for_db() {
-    echo "Waiting for PostgreSQL to be ready..."
-    export PGPASSWORD="$POSTGRES_PASSWORD"
-    while ! pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_DB" > /dev/null 2>&1; do
-        echo "PostgreSQL is unavailable - sleeping"
-        sleep 2
-    done
-    echo "PostgreSQL is up - continuing"
-}
+# # Function to check database readiness
+# function wait_for_db() {
+#     echo "Waiting for PostgreSQL to be ready..."
+#     export PGPASSWORD="$POSTGRES_PASSWORD"
+#     while ! pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER" -d "$POSTGRES_DB" > /dev/null 2>&1; do
+#         echo "PostgreSQL is unavailable - sleeping"
+#         sleep 2
+#     done
+#     echo "PostgreSQL is up - continuing"
+# }
 
-# Wait for the database to be ready
-wait_for_db
+# # Wait for the database to be ready
+# wait_for_db
 
 # Run database migrations
 echo "Running migrations..."
