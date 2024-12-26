1313f737611948ae2e2609f744419e0c70658f8a
Super-Nova-Software <supernovasoftwareco@gmail.com>
2024-12-26 06:53:08 +0300

    Add static file serving for media in development environment

--- painfx_backend/backend/core/urls.py
+++ painfx_backend/backend/core/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-] + rest_api_urlpatterns
+] + rest_api_urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Serve static and media files in development
 if settings.DEBUG:
