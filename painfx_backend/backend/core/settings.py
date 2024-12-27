a875043feaee835a3417c36258d7e24446d3fd89
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2024-12-27 17:44:40 +0300

    Update settings.py

--- painfx_backend/backend/core/settings.py
+++ painfx_backend/backend/core/settings.py
@@ -20,8 +20,8 @@ try:
 except OSError as e:
     raise ImproperlyConfigured(f"Unable to create log directory: {e}")
 
-DEBUG = env("DJANGO_DEBUG", default=False)
-DEVELOPMENTMODE = env("DEVELOPMENTMODE", default=False)
+DEBUG = env("DJANGO_DEBUG", default=True)
+DEVELOPMENTMODE = env("DEVELOPMENTMODE", default=True)
 
 # Secret key
 SECRET_KEY = env('DJANGO_SECRET_KEY', default=get_random_secret_key())
