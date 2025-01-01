fd2a93c2bb445121963a398d6b2c981f64e52945
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2025-01-01 23:34:52 +0300

    Update authentication.py

--- painfx_backend/backend/apps/authentication/authentication.py
+++ painfx_backend/backend/apps/authentication/authentication.py
@@ -1,52 +1,22 @@
 from django.conf import settings
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-def get_tokens_for_user(user):
-    refresh = RefreshToken.for_user(user)
-    return {
-        'refresh': str(refresh),
-        'access': str(refresh.access_token),
-    }
 
-def set_auth_cookies(response, tokens, platform='web'):
-    if platform == 'web':
-        response.set_cookie(
-            'access',
-            tokens['access'],
-            max_age=settings.AUTH_COOKIE_MAX_AGE,
-            path=settings.AUTH_COOKIE_PATH,
-            secure=settings.AUTH_COOKIE_SECURE,
-            httponly=settings.AUTH_COOKIE_HTTP_ONLY,
-            samesite=settings.AUTH_COOKIE_SAMESITE
-        )
-        response.set_cookie(
-            'refresh',
-            tokens['refresh'],
-            max_age=settings.AUTH_COOKIE_MAX_AGE,
-            path=settings.AUTH_COOKIE_PATH,
-            secure=settings.AUTH_COOKIE_SECURE,
-            httponly=settings.AUTH_COOKIE_HTTP_ONLY,
-            samesite=settings.AUTH_COOKIE_SAMESITE
-        )
-    else:  # mobile (iOS, Android)
-        response.set_cookie(
-            'access',
-            tokens['access'],
-            max_age=settings.MOBILE_COOKIE_MAX_AGE,
-            secure=settings.MOBILE_AUTH_COOKIE_SECURE,
-            httponly=settings.MOBILE_AUTH_COOKIE_HTTP_ONLY,
-            samesite=settings.MOBILE_AUTH_COOKIE_SAMESITE
-        )
-        response.set_cookie(
-            'refresh',
-            tokens['refresh'],
-            max_age=settings.MOBILE_COOKIE_MAX_AGE,
-            secure=settings.MOBILE_AUTH_COOKIE_SECURE,
-            httponly=settings.MOBILE_AUTH_COOKIE_HTTP_ONLY,
-            samesite=settings.MOBILE_AUTH_COOKIE_SAMESITE
-        )
+class CustomJWTAuthentication(JWTAuthentication):
+    def authenticate(self, request):
+        try:
+            header = self.get_header(request)
 
-def clear_auth_cookies(response):
-    response.delete_cookie('access')
-    response.delete_cookie('refresh')
+            if header is None:
+                raw_token = request.COOKIES.get(settings.AUTH_COOKIE)
+            else:
+                raw_token = self.get_raw_token(header)
 
+            if raw_token is None:
+                return None
+
+            validated_token = self.get_validated_token(raw_token)
+
+            return self.get_user(validated_token), validated_token
+        except:
+            return None
