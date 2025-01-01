69654f26d5dbeaa0815f2ea59893a1283208300c
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2025-01-01 23:27:31 +0300

    Update use-login.ts

--- painfx_frontend/src/hooks/auth/use-login.ts
+++ painfx_frontend/src/hooks/auth/use-login.ts
@@ -27,7 +27,7 @@ export default function useLogin() {
   const onAuthenticateUser = handleSubmit(async (values) => {
     try {
       await toast.promise(
-        login({ email: values.email, password: values.password }).unwrap(),
+        login({ email: values.email, password: values.password,platform: "web" }).unwrap(),
         {
           loading: "Logging in...",
           success: () => {
