fba6c1dd559f8daa9460ca157c8ca45463fde676
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2025-01-06 23:12:37 +0300

    Update apps.py

--- painfx_backend/backend/apps/booking_app/apps.py
+++ painfx_backend/backend/apps/booking_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BookingAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.booking_app'
+    
+    def ready(self):
+        import apps.booking_app.signals
