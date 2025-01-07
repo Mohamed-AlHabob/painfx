ae7ea0202dc12391d60d4e4ab21f2bb27b359763
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2025-01-07 17:06:31 +0300

    Update post.ts

--- painfx_frontend/src/schemas/Social/post.ts
+++ painfx_frontend/src/schemas/Social/post.ts
@@ -13,12 +13,20 @@ export const media_attachmentsSchema = z.object({
 
 export type MediaAttachments = z.infer<typeof media_attachmentsSchema>;
 
+export const TagSchema = z.object({
+  id: z.string().uuid().optional(),
+  name: z.string().nullable().optional(),
+});
+
+export type Tag = z.infer<typeof TagSchema>;
+
 export const postSchema = z.object({
     id: z.string().uuid().optional(),
     title: z.string().nullable().optional(),
     doctor: doctorSchema.optional(),
     content: z.string().nullable().optional(),
     media_attachments: z.array(media_attachmentsSchema).nullable().optional(),
+    tag: z.array(TagSchema).nullable().optional(),
     comments_count: z.number().nullable().optional(),
     likes_count: z.number().nullable().optional(),
     created_at: z.string().datetime().nullable().optional(),
