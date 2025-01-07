deb232da8823506925c3d7b747f734fa1d80eef3
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2025-01-07 22:21:53 +0300

    Update signals.py

--- painfx_backend/backend/apps/booking_app/signals.py
+++ painfx_backend/backend/apps/booking_app/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from apps.booking_app.models import Post, Reservation ,Notification
+from apps.booking_app.models import Post, Reservation, Notification
 
 @receiver(post_save, sender=Post)
 def send_like_notification(sender, instance, created, **kwargs):
@@ -8,16 +8,16 @@ def send_like_notification(sender, instance, created, **kwargs):
         return
     
     if instance.likes.count() > 0:  # Assuming you have a `likes` ManyToMany relationship
-        for user in instance.likes.all():
+        for like in instance.likes.all():
             # Create a like notification
             Notification.objects.create(
-                user=user,
-                content=f"Your post has been liked by {instance.user.first_name}",
+                user=like.user,  # Access the user through the like
+                message=f"Your post has been liked by {like.user.first_name}",
                 notification_type='like'
             )
 
 @receiver(post_save, sender=Reservation)
-def send_Reservation_notification(sender, instance, created, **kwargs):
+def send_reservation_notification(sender, instance, created, **kwargs):
     if created:
         return
     
@@ -29,7 +29,7 @@ def send_Reservation_notification(sender, instance, created, **kwargs):
         status_message = 'Your booking has been declined.'
 
     Notification.objects.create(
-        user=instance.patient,  # Assuming Booking has a `patient` field pointing to User
-        content=status_message,
+        user=instance.patient.user,  # Access the user through the patient
+        message=status_message,
         notification_type='booking'
     )
