3bfb70c9aaaa9bdc5c6744529545bbdc25d75d58
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2025-01-07 20:55:56 +0300

    Update settings.py

--- painfx_backend/backend/core/settings.py
+++ painfx_backend/backend/core/settings.py
@@ -110,10 +110,10 @@ ASGI_APPLICATION = "core.asgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('POSTGRES_DB', default='painfx_datebase_yoz7'),
-        'USER': env('POSTGRES_USER', default='painfx_datebase_yoz7_user'),
-        'PASSWORD': env('POSTGRES_PASSWORD', default='Rewg19iYzQT16vtpWV936biIEj1iVPOo'),
-        "HOST": env("POSTGRES_HOST", default="dpg-ctujn2jtq21c73bjbbig-a"),
+        'NAME': env('POSTGRES_DB', default='painfx_datebase_hk8n'),
+        'USER': env('POSTGRES_USER', default='painfx_datebase_hk8n_user'),
+        'PASSWORD': env('POSTGRES_PASSWORD', default='fRbNWvfvltY7lbCiCQ2zXTtwHYA8zU5u'),
+        "HOST": env("POSTGRES_HOST", default="dpg-ctumkia3esus739ebkig-a"),
         "PORT": env("POSTGRES_PORT", default="5432"),
     }
 }
