8ad762a63c55ddb5178675870a2aa1dbed2ab70a
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2025-01-18 12:40:21 +0300

    Update TranslationsProvider.tsx

--- painfx_frontend/src/components/providers/TranslationsProvider.tsx
+++ painfx_frontend/src/components/providers/TranslationsProvider.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { ReactNode, useEffect, useMemo } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 import { I18nextProvider } from 'react-i18next'
 import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next/initReactI18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { getOptions, languages } from '@/i18n/settings'
+
 
 interface TranslationsProviderProps {
   children: ReactNode
@@ -16,40 +17,37 @@ export default function TranslationsProvider({
   children,
   locale
 }: TranslationsProviderProps) {
-  const instance = useMemo(() => {
+  const [instance] = useState(() => {
     const i18nInstance = i18next.createInstance()
-    const storedLang = localStorage.getItem('lang') || locale
-
     i18nInstance
       .use(initReactI18next)
       .use(LanguageDetector)
       .init({
-        ...getOptions(storedLang),
-        lng: storedLang, // Use stored language or fallback to locale
+        ...getOptions(locale),
+        lng: locale,
         detection: {
           order: ['localStorage', 'htmlTag'],
           caches: ['localStorage'],
-          lookupLocalStorage: 'lang',
+       //   lookupLocalStorage: 'lang',
         },
-        resources: languages.reduce(
-          (acc: Record<string, { translation: Record<string, string> }>, lang) => {
-            acc[lang] = {
-              translation: require(`@/i18n/locales/${lang}/translation.json`)
-            }
-            return acc
-          },
-          {}
-        )
+        resources: languages.reduce((acc: { [x: string]: { translation: any } }, lang: string | number) => {
+          acc[lang] = {
+            translation: require(`@/i18n/locales/${lang}/translation.json`)
+          }
+          return acc
+        }, {} as Record<string, { translation: Record<string, string> }>)
       })
     return i18nInstance
-  }, [locale])
+  })
 
   useEffect(() => {
     const storedLang = localStorage.getItem('lang') || locale
-    if (storedLang !== instance.language) {
-      instance.changeLanguage(storedLang)
-    }
+    instance.changeLanguage(storedLang)
   }, [instance, locale])
 
-  return <I18nextProvider i18n={instance}>{children}</I18nextProvider>
+  return (
+    <I18nextProvider i18n={instance}>
+      {children}
+    </I18nextProvider>
+  )
 }
