7ef2fb9f3c19c5b35409c71f954f67dbcf88b75b
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2025-02-05 13:50:49 +0300

    fix FriendSerializer

--- painfx_backend/backend/apps/chat/serializers.py
+++ painfx_backend/backend/apps/chat/serializers.py
@@ -41,41 +41,42 @@ class RequestSerializer(serializers.ModelSerializer):
 
 
 class FriendSerializer(serializers.ModelSerializer):
-	friend = serializers.SerializerMethodField()
-	preview = serializers.SerializerMethodField()
-	updated_at = serializers.SerializerMethodField()
-	
-	class Meta:
-		model = Connection
-		fields = [
-			'id',
-			'friend',
-			'preview',
-			'updated_at'
-		]
+    friend = serializers.SerializerMethodField()
+    preview = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
-	def get_friend(self, obj):
-		# If Im the sender
-		if self.context['user'] == obj.sender:
-			return UserSerializer(obj.receiver).data
-		# If Im the receiver
-		elif self.context['user'] == obj.receiver:
-			return UserSerializer(obj.sender).data
-		else:
-			print('Error: No user found in friendserializer')
+    class Meta:
+        model = Connection
+        fields = [
+            'id',
+            'friend',
+            'preview',
+            'updated_at'
+        ]
 
-	def get_preview(self, obj):
-		default = 'New connection'
-		if not hasattr(obj, 'latest_text'):
-			return default
-		return obj.latest_text or default
+    def get_friend(self, obj):
+        # If I'm the sender
+        if self.context['user'] == obj.sender:
+            return UserSerializer(obj.receiver).data
+        # If I'm the receiver
+        elif self.context['user'] == obj.receiver:
+            return UserSerializer(obj.sender).data
+        else:
+            raise ValueError('Error: No user found in friendserializer')
 
-	def get_updated(self, obj):
-		if not hasattr(obj, 'latest_created'):
-			date = obj.updated_at
-		else:
-			date = obj.latest_created or obj.updated_at
-		return date.isoformat()
+    def get_preview(self, obj):
+        default = 'New connection'
+        if not hasattr(obj, 'latest_text'):
+            return default
+        return obj.latest_text or default
+
+    def get_updated_at(self, obj):
+        if not hasattr(obj, 'latest_created'):
+            date = obj.updated_at
+        else:
+            date = obj.latest_created or obj.updated_at
+        return date.isoformat()
+
 
 
 
