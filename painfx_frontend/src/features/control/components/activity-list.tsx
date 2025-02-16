71b337fe9e5c05623fd0cc9719d7522ca66e26e1
Mohamed Al-Habob <110397949+Mohamed-AlHabob@users.noreply.github.com>
2025-02-16 10:30:02 +0300

    Fix 21

--- painfx_frontend/src/features/control/components/activity-list.tsx
+++ painfx_frontend/src/features/control/components/activity-list.tsx
@@ -19,7 +19,7 @@ export function ActivityList({ title, doctors }: ActivityListProps) {
         {doctors.map((doctor, i) => (
           <div key={i} className="flex items-center justify-between">
             <div className="flex items-center gap-3">
-            <UserCard id={doctor.id || ""} avatar={doctor?.user?.profile?.avatar || ""} name={`${doctor.user?.first_name} ${doctor.user?.last_name}`} email={doctor.user?.email} phone_number={doctor.user?.profile?.phone_number|| ""} address={doctor.user?.profile?.address || ""} joined={doctor.user?.date_joined || ""} />
+            <UserCard id={doctor.id || ""} avatar={doctor?.user?.profile?.avatar || ""} name={`${doctor.user?.first_name} ${doctor.user?.last_name}`} email={doctor.user?.email} phone_number={doctor.user?.profile?.phone_number|| ""} address={doctor.user?.profile?.address || ""} joined={doctor.user?.created_at || ""} />
             <p className="text-sm text-muted-foreground">{""}</p>
           </div>
       </div>
